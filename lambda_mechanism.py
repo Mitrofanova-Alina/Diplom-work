c8519eea995b1fab66487d49a60310c52c6f7591
Alina Mitrofanova <alina010299@mail.ru>
2020-05-25 13:43:09 +0300

    Add some code
    - animation lambda mechanism
    - animation bearing

--- lambda_mechanism.py
+++ lambda_mechanism.py
@@ -3,6 +3,7 @@ import numpy as np
 import pylab
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptc
+import matplotlib.ticker as ticker
 from celluloid import Camera
 
 
@@ -17,7 +18,8 @@ class LambdaMechanism:
         self.delta_axis_angle = delta_axis_angle
 
     def motion(self):
-        A = self.__rotation(self.delta_axis_normal, self.delta_axis_angle, self.phi)
+        rot = self.__rotation(self.delta_axis_normal, self.delta_axis_angle)
+        A = (rot * self.l2).dot(np.array([np.cos(self.phi), np.sin(self.phi), np.zeros(len(self.phi))]))
         AB_xy = np.sqrt(self.l3 ** 2 - A[2, :] ** 2)
         AC = np.sqrt((self.l1 - A[0, :]) ** 2 + A[1, :] ** 2)
         angle_ACB = np.arccos((AC ** 2 + self.l4 ** 2 - AB_xy ** 2) / (2 * AC * self.l4))
@@ -31,8 +33,8 @@ class LambdaMechanism:
         M = 2 * B - A
         BA = B - A
         n = np.sqrt(BA[0, :] ** 2 + BA[1, :] ** 2)
-        delta_B_normal = [- BA[1, :] / n, BA[0, :] / n, np.zeros(len(self.phi))]
-        delta_B_angle = np.arctan(BA[2, :] / n)
+        delta_B_normal = np.array([- BA[1, :] / n, BA[0, :] / n, np.zeros(len(self.phi))])
+        delta_B_angle = np.array(np.arctan(BA[2, :] / n))
         return A, B, M, delta_B_normal, delta_B_angle
 
     def draw_lambda_mechanism_motion_2d(self, A, M, B):
@@ -58,7 +60,6 @@ class LambdaMechanism:
             ax.plot([A[0, i]], [A[1, i]], "o", color='grey', linewidth=1)
             ax.plot([B[0, i]], [B[1, i]], "o", color='grey', linewidth=1)
             ax.plot([M[0, i]], [M[1, i]], "o", color='grey', linewidth=1)
-
             camera.snap()
 
         anim = camera.animate()
@@ -95,7 +96,58 @@ class LambdaMechanism:
         anim.save("lambda animation 3d.gif", writer="imagemagick")
         plt.close(fig)
 
-    def __rotation(self, n, alpha, phi):
+    def __calculate_bearing_animation(self, i, A, B, delta_B_normal, delta_B_angle, cylinder, Hb, Rb, Rh, ax):
+        rotB = self.__rotation(delta_B_normal[:, i], delta_B_angle[i])
+        cylinder_rotation = rotB.dot(cylinder)
+        cylinder_rotation_out = [[], [], []]
+        for j in range(len(cylinder_rotation[0])):
+            if cylinder_rotation[0][j] ** 2 + cylinder_rotation[1][j] ** 2 > Rh ** 2:
+                cylinder_rotation_out[0].append(cylinder_rotation[0][j])
+                cylinder_rotation_out[1].append(cylinder_rotation[1][j])
+                cylinder_rotation_out[2].append(cylinder_rotation[2][j])
+        cylinder_rotation_out = np.array(cylinder_rotation_out)
+
+        BC = np.array([self.l1, 0, 0]).T - B[:, i]
+        BC = BC * (Rb / np.sqrt(np.linalg.norm(BC.T.dot(BC))))
+        BA = A[:, i] - B[:, i]
+        BA = Rb * BA / np.linalg.norm(BA)
+
+        ax.plot(cylinder_rotation_out[0, :], cylinder_rotation_out[1, :], cylinder_rotation_out[2, :], ".", color='red')
+        ax.plot([0, 0], [0, 0], [- Hb / 2, Hb / 2], 'b')
+        ax.plot([0, BC[0]], [0, BC[1]], [- Hb / 2, - Hb / 2 + BC[2]], 'blue')
+        ax.plot([0, BA[0]], [0, BA[1]], [- Hb / 2, - Hb / 2 + BA[2]], 'red')
+        ax.plot([0, BC[0]], [0, BC[1]], [Hb / 2, Hb / 2 + BC[2]], 'blue')
+        ax.plot([0, BA[0]], [0, BA[1]], [Hb / 2, Hb / 2 + BA[2]], 'red')
+        ax.plot(Rb * np.cos(self.phi), Rb * np.sin(self.phi), np.zeros(len(self.phi)) - Hb / 2, 'blue')
+        ax.plot(Rb * np.cos(self.phi), Rb * np.sin(self.phi), np.zeros(len(self.phi)) + Hb / 2, 'blue')
+
+    def draw_bearing_animation_3d(self, A, M, B, delta_B_normal, delta_B_angle):
+        fig = pylab.figure()
+        ax = Axes3D(fig)
+        ax.set_title("Bearing mechanism 3d animation")
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        camera = Camera(fig)
+
+        Rb = 17.5
+        Hb = 10
+        Rbh = 0.03
+        Rh = Rb + Rbh
+        h = np.linspace(- Hb / 2, Hb / 2, 30)
+        cylinder = np.array([Rb * np.kron(np.cos(self.phi), np.ones((1, len(h))))[0],
+             Rb * np.kron(np.sin(self.phi), np.ones((1, len(h))))[0],
+             np.kron(np.ones((1, len(self.phi))), h)[0]])
+
+        for i in range(len(delta_B_angle)):
+            self.__calculate_bearing_animation(i, A, B, delta_B_normal, delta_B_angle, cylinder, Hb, Rb, Rh, ax)
+            camera.snap()
+
+        anim = camera.animate()
+        anim.save("Bearing mechanism animation 3d.gif", writer="imagemagick")
+        plt.close(fig)
+
+    def __rotation(self, n, alpha):
         c = np.cos(alpha)
         s = np.sin(alpha)
 
@@ -107,5 +159,4 @@ class LambdaMechanism:
             n[0] * n[2] * (1 - c) + n[1] * s, n[1] * n[2] * (1 - c) - n[0] * s, c + (n[2] ** 2) * (1 - c)
         ]])
 
-        A = (rot * self.l2).T.dot(np.array([np.cos(phi), np.sin(phi), np.zeros(len(phi))]))
-        return A
+        return rot.T
--- main.py
+++ main.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
 
     size = 1
     delta_axis_normal = [1, 0, 0]
-    delta_axis_angle = 20 / 60
+    delta_axis_angle = 2 / 60
     phi0 = np.pi / 2
     phi = np.linspace(phi0, phi0 + 4 * np.pi, 100)
     lambda_mechanism = LambdaMechanism(size, phi, delta_axis_normal, delta_axis_angle)
     A, B, M, delta_B_normal, delta_B_angle = lambda_mechanism.motion()
-    lambda_mechanism.draw_lambda_mechanism_motion_2d(A, M, B)
+    # lambda_mechanism.draw_lambda_mechanism_motion_2d(A, M, B)
     lambda_mechanism.draw_lambda_mechanism_motion_3d(A, M, B)
+    lambda_mechanism.draw_bearing_animation_3d(A, M, B, delta_B_normal, delta_B_angle)
 
     print("Bye, world!")
